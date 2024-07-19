412bccc74e4e16373b8094abe90f870eec336227
Leandro Dias Sanches <sanches.l.d@gmail.com>
2024-07-19 15:37:44 -0300

    modified:   problemas/problema_1.py

--- problemas/problema_1.py
+++ problemas/problema_1.py
@@ -1,12 +1,12 @@
 '''
-# Problema 1: Análise de Composição de Nucleotídeos
+Problema 1: Análise de Composição de Nucleotídeos
 
-# Tarefa: Escreva um script que:
+Tarefa: Escreva um script que:
 
-# 1) Faça o parse do arquivo multiFASTA, usando a função ler_fasta.
-# 2) Imprima o percentual de cada nucleotídeo (A, T, C, G) e o conteúdo GC (percentual de C + G) para cada sequência.
+1) Faça o parse do arquivo multiFASTA, usando a função ler_fasta.
+2) Imprima o percentual de cada nucleotídeo (A, T, C, G) e o conteúdo GC (percentual de C + G) para cada sequência.
 
-# Dica: Use sua classe Sequencia e sua função .calcular_percentual(bases)
+Dica: Use sua classe Sequencia e sua função .calcular_percentual(bases)
 '''
 
 from bio.ler_fasta import ler_fasta
@@ -22,6 +22,6 @@ for organismo in organismos:
     }
 
     print(f"Percentual de cada nucleotídeo da sequencia de ${organismo.get("nome", "")}:\n")
-    
+
     for percentual_nucleotideo in percentual_nucleotideos:
         print(f"${percentual_nucleotideo}: ${percentual_nucleotideos[percentual_nucleotideo]}")
